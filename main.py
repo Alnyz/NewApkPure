afe878625d9d4a84c2b307bf8ae5e0cc343c7202
Dyseo <katro.coplax@gmail.com>
2023-03-01 01:43:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from core.api import Api
 
 api = Api()
-links = [
-    'https://d.apkpure.com/b/APK/com.xfx.surfvpn?version=latest',
-    'https://d.apkpure.com/b/APK/net.bypass.vpn?version=latest',
-    'https://d.apkpure.com/b/APK/free.vpn.filter.unblock.proxy.hotspot.fastvpn?version=latest'
-]
 
-# y = api.search('vpn', first=False)
-# api.download(y, count=3)
-# print(api.download(1, index=2, count=2))
+y = api.search('vpn', first=False)
+api.download(y, count=3)
